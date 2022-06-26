e9fd8317a29700aee6043c38338db0a2f9c79229
“Shiwanshu <skumar@hawk.iit.edu>
2022-06-26 15:22:12 -0500

    manage py seetings update

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NLCWeb.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_base.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
